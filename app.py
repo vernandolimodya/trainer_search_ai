8ec50cc004930465ba958de1798e0c80666a922c
Vernando Limodya <vernandolimodya99@gmail.com>
2025-03-17 16:25:25 +0100

    Edit requirements.txt and changed hostname and port number for render

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ async def show_results(request: Request, query: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
